41bded76ab9f20f0124d8fa7fe38b6787c3233de
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-08-20 21:24:26 -0300

    Added logout feature and user model spec

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -17,4 +17,22 @@ feature 'User is able to log in' do
     expect(page).to have_content('Sair')
     expect(page).not_to have_content('Entrar')
   end
+
+  scenario 'and then log out' do
+    # Arrange
+    User.create(email: 'alan@email.com', password: '123456')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'alan@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Logar'
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content('Entrar')
+    expect(page).not_to have_content("Você esta logado como: alan@email.com")
+    expect(page).not_to have_content('Sair')
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:email).of_type(:string) }
+  it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+  it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
 end
