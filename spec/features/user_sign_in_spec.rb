a6aff23e064c8be3917b1f375adda75d4de98cf1
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-08-20 21:18:45 -0300

    Added devise gem, user model and signup and signin features

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User is able to log in' do
+  scenario 'successfully' do
+    # Arrange
+    User.create(email: 'alan@email.com', password: '123456')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'alan@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Logar'
+
+    # Assert
+    expect(page).to have_content("Você esta logado como: alan@email.com")
+    expect(page).to have_content('Sair')
+    expect(page).not_to have_content('Entrar')
+  end
+end
