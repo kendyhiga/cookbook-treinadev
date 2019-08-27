92e5c72c36ac710a5b88781111c4bb94d2867e72
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-08-26 23:34:37 -0300

    added test

--- spec/features/admin_must_approve_or_reject_recipe_spec.rb
+++ spec/features/admin_must_approve_or_reject_recipe_spec.rb
@@ -49,4 +49,26 @@ feature 'Admin must approve or reject recipe' do
 
     expect(page).to have_css('p', text: 'Bolo de fubá')
   end
+
+  scenario 'regular user cant see accept/reject buttons' do
+    recipe_type = RecipeType.create!(name: 'Sobremesa')
+    cuisine = Cuisine.create!(name: 'Brasileira')
+    user = User.create!(email: 'user@email.com', password: '123456')
+    Recipe.create!(title: 'Bolo de fubá', difficulty: 'Médio',
+                   recipe_type: recipe_type, cuisine: cuisine,
+                   cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                   cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos,\
+                                 misture com o restante dos ingredientes',
+                   user: user, status: 'accepted')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Logar'
+    click_on 'Bolo de fubá'
+
+    expect(page).not_to have_content('Aceitar')
+    expect(page).not_to have_content('Rejeitar')
+  end
 end
