d0588c3276314c6d77bcf958ff14be96763101ca
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-09-25 22:34:31 -0300

    added my profile feature tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    redirect_to root_path unless current_user == @user
+
     @recipes = Recipe.where(user: @user)
   end
 
