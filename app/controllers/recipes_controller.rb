f28b97cd93c448ec3743810934687514de4c34a9
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-10-21 18:55:40 -0300

    using build method instead

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,8 +27,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user = current_user
+    @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
       redirect_to @recipe
     else
